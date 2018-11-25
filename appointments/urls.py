c75af7d9d8969446e343517a3f635ec41fbd4222
king <kingjavanlee@hotmail.com>
2018-11-25 01:29:43 -0600

    feat: initial

---
+++ appointments/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.conf.urls import url, include
+from rest_framework import routers
+from users.views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
